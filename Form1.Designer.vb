e45ae2a1f1bb82dd6894361c01c975d9ccb8a6e9
Toshinobu Hashizume <toshinobu.hashizume@gmail.com>
2015-05-11 23:44:59 +0900

    auto commit 20150511

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,21 +22,33 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AnalyzeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FileToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DirectoryNameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExtentsNameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ExtentsNameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.DirectoryNameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SELECTToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ZipArchiveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MoveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         Me.MenuStrip1.SuspendLayout()
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.Panel1.SuspendLayout()
         Me.SplitContainer1.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -51,7 +63,7 @@ Partial Class Form1
         '
         'FileToolStripMenuItem
         '
-        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AnalyzeToolStripMenuItem})
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AnalyzeToolStripMenuItem, Me.ExitToolStripMenuItem})
         Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
         Me.FileToolStripMenuItem.Size = New System.Drawing.Size(40, 22)
         Me.FileToolStripMenuItem.Text = "&File"
@@ -59,8 +71,39 @@ Partial Class Form1
         'AnalyzeToolStripMenuItem
         '
         Me.AnalyzeToolStripMenuItem.Name = "AnalyzeToolStripMenuItem"
-        Me.AnalyzeToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AnalyzeToolStripMenuItem.Size = New System.Drawing.Size(121, 22)
         Me.AnalyzeToolStripMenuItem.Text = "&Analyze"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(121, 22)
+        Me.ExitToolStripMenuItem.Text = "&Exit"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem1, Me.DirectoryNameToolStripMenuItem, Me.ExtentsNameToolStripMenuItem})
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(48, 22)
+        Me.ViewToolStripMenuItem.Text = "&View"
+        '
+        'FileToolStripMenuItem1
+        '
+        Me.FileToolStripMenuItem1.Name = "FileToolStripMenuItem1"
+        Me.FileToolStripMenuItem1.Size = New System.Drawing.Size(130, 22)
+        Me.FileToolStripMenuItem1.Text = "&File"
+        '
+        'DirectoryNameToolStripMenuItem
+        '
+        Me.DirectoryNameToolStripMenuItem.Name = "DirectoryNameToolStripMenuItem"
+        Me.DirectoryNameToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.DirectoryNameToolStripMenuItem.Text = "&Directory"
+        '
+        'ExtentsNameToolStripMenuItem
+        '
+        Me.ExtentsNameToolStripMenuItem.Name = "ExtentsNameToolStripMenuItem"
+        Me.ExtentsNameToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.ExtentsNameToolStripMenuItem.Text = "&Extents"
         '
         'StatusStrip1
         '
@@ -76,11 +119,11 @@ Partial Class Form1
         Me.SplitContainer1.Location = New System.Drawing.Point(0, 28)
         Me.SplitContainer1.Name = "SplitContainer1"
         '
-        'SplitContainer1.Panel2
+        'SplitContainer1.Panel1
         '
-        Me.SplitContainer1.Panel2.Controls.Add(Me.DataGridView1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.DataGridView1)
         Me.SplitContainer1.Size = New System.Drawing.Size(980, 443)
-        Me.SplitContainer1.SplitterDistance = 326
+        Me.SplitContainer1.SplitterDistance = 842
         Me.SplitContainer1.TabIndex = 2
         '
         'DataGridView1
@@ -89,32 +132,52 @@ Partial Class Form1
         Me.DataGridView1.AllowUserToDeleteRows = False
         Me.DataGridView1.AllowUserToOrderColumns = True
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.ContextMenuStrip = Me.ContextMenuStrip1
         Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.Black
+        Me.DataGridView1.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
         Me.DataGridView1.RowTemplate.Height = 21
-        Me.DataGridView1.Size = New System.Drawing.Size(650, 443)
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(842, 443)
         Me.DataGridView1.TabIndex = 0
         '
-        'ViewToolStripMenuItem
+        'ContextMenuStrip1
         '
-        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExtentsNameToolStripMenuItem, Me.DirectoryNameToolStripMenuItem})
-        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
-        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(48, 22)
-        Me.ViewToolStripMenuItem.Text = "&View"
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SELECTToolStripMenuItem, Me.ToolStripSeparator1, Me.ZipArchiveToolStripMenuItem, Me.DeleteToolStripMenuItem, Me.MoveToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(142, 98)
         '
-        'ExtentsNameToolStripMenuItem
+        'SELECTToolStripMenuItem
         '
-        Me.ExtentsNameToolStripMenuItem.Name = "ExtentsNameToolStripMenuItem"
-        Me.ExtentsNameToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
-        Me.ExtentsNameToolStripMenuItem.Text = "&Extents Name"
+        Me.SELECTToolStripMenuItem.Name = "SELECTToolStripMenuItem"
+        Me.SELECTToolStripMenuItem.Size = New System.Drawing.Size(141, 22)
+        Me.SELECTToolStripMenuItem.Text = "SELECT"
         '
-        'DirectoryNameToolStripMenuItem
+        'ToolStripSeparator1
         '
-        Me.DirectoryNameToolStripMenuItem.Name = "DirectoryNameToolStripMenuItem"
-        Me.DirectoryNameToolStripMenuItem.Size = New System.Drawing.Size(169, 22)
-        Me.DirectoryNameToolStripMenuItem.Text = "&Directory Name"
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(138, 6)
+        '
+        'ZipArchiveToolStripMenuItem
+        '
+        Me.ZipArchiveToolStripMenuItem.Name = "ZipArchiveToolStripMenuItem"
+        Me.ZipArchiveToolStripMenuItem.Size = New System.Drawing.Size(141, 22)
+        Me.ZipArchiveToolStripMenuItem.Text = "&Zip Archive"
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(141, 22)
+        Me.DeleteToolStripMenuItem.Text = "&Delete"
+        '
+        'MoveToolStripMenuItem
+        '
+        Me.MoveToolStripMenuItem.Name = "MoveToolStripMenuItem"
+        Me.MoveToolStripMenuItem.Size = New System.Drawing.Size(141, 22)
+        Me.MoveToolStripMenuItem.Text = "&Move"
         '
         'Form1
         '
@@ -131,10 +194,11 @@ Partial Class Form1
         Me.Text = "Form1"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
-        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainer1.ResumeLayout(False)
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -149,5 +213,15 @@ Partial Class Form1
     Friend WithEvents ViewToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents ExtentsNameToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents DirectoryNameToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FileToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents SELECTToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ZipArchiveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MoveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,36 +1,151 @@
 ﻿Public Class Form1
 
-    Dim _dt As System.Data.DataTable = Nothing
-    Dim _distExt As System.Data.DataTable = Nothing
-    Dim _distDirectory As System.Data.DataTable = Nothing
+    Private _dt As System.Data.DataTable = Nothing
+    Private _distExt As System.Data.DataTable = Nothing
+    Private _distDirectory As System.Data.DataTable = Nothing
     Private _fl As jp.polestar.io.FileList = New jp.polestar.io.FileList
+    Private _conditionString As String = String.Empty
+    Private _countFile As Long = 0
+    Private _countSize As Long = 0
+    Private _dvType As ENUM_DV_TYPE = ENUM_DV_TYPE.file
 
     Private Sub AnalyzeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AnalyzeToolStripMenuItem.Click
-        _dt = _fl.Analyse("c:\Users\toshi")
-        Me.DataGridView1.DataSource = _dt
+
+        Me.FolderBrowserDialog1.ShowNewFolderButton = False
+        If Me.FolderBrowserDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            ' TODO: Use BackgroundProcess
+            _dt = _fl.Analyse(Me.FolderBrowserDialog1.SelectedPath)
+
+            Dim dv1 As System.Data.DataView = New System.Data.DataView(_fl.Files, _conditionString, "SELECTED desc, DIRECTORY_NAME", DataViewRowState.CurrentRows)
+            DataGridView1.Columns.Clear()
+            '     DataGridView1.Columns(0).Visible = False
+            DataGridView1.AutoGenerateColumns = True
+            DataGridView1.DataSource = dv1
+
+            _countFile = dv1.Count
+            _countSize = dv1.ToTable().Compute("Sum(size)", String.Empty)
+            _dvType = ENUM_DV_TYPE.file
+
+
+
+        End If
+
     End Sub
 
-    ''' <summary>
-    ''' 拡張子毎に集計した結果を　DataGridView に出力する
-    ''' </summary>
-    ''' <param name="sender"></param>
-    ''' <param name="e"></param>
-    ''' <remarks></remarks>
     Private Sub ExtentsNameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExtentsNameToolStripMenuItem.Click
-        Dim dv1 As System.Data.DataView = New System.Data.DataView(_fl.Extents, String.Empty, "SIZE desc", DataViewRowState.CurrentRows)
+        Dim dv1 As System.Data.DataView = New System.Data.DataView(_fl.Extents, _conditionString, "SIZE desc", DataViewRowState.CurrentRows)
+        If dv1.Count = 0 Then Return
+
+        DataGridView1.Columns.Clear()
+        DataGridView1.AutoGenerateColumns = True
         DataGridView1.DataSource = dv1
+
+        _countFile = dv1.Count
+        _countSize = dv1.ToTable().Compute("Sum(size)", String.Empty)
+        _dvType = ENUM_DV_TYPE.ext
+
 
     End Sub
 
-    ''' <summary>
-    ''' Directory 毎にファイルサイズを集計し DataGridviewに出力する
-    ''' </summary>
-    ''' <param name="sender"></param>
-    ''' <param name="e"></param>
-    ''' <remarks></remarks>
     Private Sub DirectoryNameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DirectoryNameToolStripMenuItem.Click
-        Dim dv1 As System.Data.DataView = New System.Data.DataView(_fl.Directories, String.Empty, "SIZE desc", DataViewRowState.CurrentRows)
+        Dim dv1 As System.Data.DataView = New System.Data.DataView(_fl.Directories, _conditionString, "SIZE desc", DataViewRowState.CurrentRows)
+        If dv1.Count = 0 Then Return
+
+        DataGridView1.Columns.Clear()
+
+        DataGridView1.AutoGenerateColumns = True
         DataGridView1.DataSource = dv1
+
+        _countFile = dv1.Count
+        _countSize = dv1.ToTable().Compute("Sum(size)", String.Empty)
+        _dvType = ENUM_DV_TYPE.directory
+
+
+    End Sub
+
+    Private Sub FileToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles FileToolStripMenuItem1.Click
+        Dim dv1 As System.Data.DataView = New System.Data.DataView(_fl.Files, _conditionString, "SELECTED desc, DIRECTORY_NAME", DataViewRowState.CurrentRows)
+        If dv1.Count = 0 Then Return
+
+        DataGridView1.Columns.Clear()
+        DataGridView1.AutoGenerateColumns = True
+        DataGridView1.DataSource = dv1
+
+        _countFile = dv1.Count
+        _countSize = dv1.ToTable().Compute("Sum(size)", String.Empty)
+        _dvType = ENUM_DV_TYPE.file
+
+    End Sub
+
+    Enum ENUM_DV_TYPE As Long
+        file = 0
+        directory = 1
+        ext = 2
+    End Enum
+
+    Private Sub SELECTToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SELECTToolStripMenuItem.Click
+
+        Select Case _dvType
+            Case ENUM_DV_TYPE.file
+                Dim f As List(Of String) = New List(Of String)
+
+                For Each _row As DataGridViewRow In DataGridView1.SelectedRows
+                    f.Add(_row.Cells("FILE_NAME").Value)
+                Next
+                _fl.SeleteFile(f.ToArray)
+
+
+                Dim dv1 As System.Data.DataView = New System.Data.DataView(_fl.Files, _conditionString, "SELECTED desc, DIRECTORY_NAME", DataViewRowState.CurrentRows)
+                DataGridView1.Columns.Clear()
+                '     DataGridView1.Columns(0).Visible = False
+                DataGridView1.AutoGenerateColumns = True
+                DataGridView1.DataSource = dv1
+
+                _countFile = dv1.Count
+                _countSize = dv1.ToTable().Compute("Sum(size)", String.Empty)
+                _dvType = ENUM_DV_TYPE.file
+
+            Case ENUM_DV_TYPE.directory
+
+
+
+            Case ENUM_DV_TYPE.ext
+
+
+
+        End Select
+
+
+    End Sub
+
+
+
+    Private Sub DataGridView1_CellFormatting(sender As Object, e As DataGridViewCellFormattingEventArgs) Handles DataGridView1.CellFormatting
+        Dim dgv As DataGridView = CType(sender, DataGridView)
+        'セルの列を確認
+        If dgv.Columns(e.ColumnIndex).Name = "SELECTED" AndAlso TypeOf e.Value Is Long Then
+
+            Select Case _dvType
+                Case ENUM_DV_TYPE.file
+                    Dim val As Integer = CInt(e.Value)
+                    'セルの値により、背景色を変更する
+                    If val = 0 Then
+                        For Each _cell As DataGridViewCell In dgv.Rows(e.RowIndex).Cells
+                            _cell.Style.BackColor = Color.White
+                        Next
+                    ElseIf val = 1 Then
+                        For Each _cell As DataGridViewCell In dgv.Rows(e.RowIndex).Cells
+                            _cell.Style.BackColor = Color.WhiteSmoke
+                        Next
+                    End If
+
+
+                Case Else
+
+            End Select
+
+        End If
 
     End Sub
 End Class
+
